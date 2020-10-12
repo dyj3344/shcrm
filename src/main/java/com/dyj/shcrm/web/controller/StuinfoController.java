20f647314f4f038c528ef1661c55510a098057d7
dyj3344 <535277189@qq.com>
2020-10-12 16:35:24 +0800

    10/12 16:35 实现了导航切换

--- src/main/java/com/dyj/shcrm/web/controller/StuinfoController.java
+++ src/main/java/com/dyj/shcrm/web/controller/StuinfoController.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
-
-    @RequestMapping("/index")
+public class StuinfoController {
+    @RequestMapping("/stuinfo")
     public String index(){
-        return "index";
+        return "stuinfo/list";
     }
-
 }
